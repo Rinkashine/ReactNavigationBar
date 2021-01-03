4f87d3aa8404a9173f71250e0dd632c0532748e3
Rinkashine <75567018+Rinkashine@users.noreply.github.com>
2021-01-03 13:01:07 +0800

    React

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,7 @@
+export default [
+  { id: 1, title: "Home", cName: "nav-link" },
+  { id: 2, title: "About", cName: "nav-link" },
+  { id: 3, title: "Services", cName: "nav-link" },
+  { id: 4, title: "Contact", cName: "nav-link" },
+  { id: 5, title: "Feedback", cName: "nav-link" },
+];
